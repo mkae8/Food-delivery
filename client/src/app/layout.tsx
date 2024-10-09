5dee293d59b7a3133fca35c664be6b1de4a0c9ce
Bilguun <Bilguun040728@gmail.com>
2024-10-09 12:30:28 +0800

    footer arai duusaguen

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { MuiProvider } from "@/provider/MuiProvider";
+import Footer from "@/components/footer/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,7 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <MuiProvider>{children} </MuiProvider>
+        <MuiProvider>
+          {children}
+          <Footer />
+        </MuiProvider>
       </body>
     </html>
   );
---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,140 @@
+// "use client";
+// import style from "";
+// import { Typography } from "@mui/material";
+
+// const Footer = () => {
+//   return (
+//     <div className="footer">
+//       <div>
+//         <img src="/image.png" alt="" />
+//         <h2>Food Delivery</h2>
+//       </div>
+//       <div>
+//         <Typography color="text.secondary">
+//           <a href="">Home</a>
+//         </Typography>
+
+//         <Typography color="text.secondary">
+//           <a href="">Холбоо барих</a>
+//         </Typography>
+
+//         <Typography color="text.secondary">
+//           <a href="">Хоолны цэс</a>
+//         </Typography>
+
+//         <Typography color="text.secondary">
+//           <a href="">Үйлчилгээний нөхцөл</a>
+//         </Typography>
+//         <Typography color="text.secondary">
+//           <a href="">Хүргэлтийн бүс</a>
+//         </Typography>
+//         <Typography color="text.secondary">
+//           <a href="">Нууцлалын бодлого</a>
+//         </Typography>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Footer;
+
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Image from "next/image";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Link from "next/link";
+
+export default function GuestFooter() {
+  return (
+    <Paper
+      sx={{ marginTop: "calc(10% + 100px)", bottom: 0 }}
+      component="footer"
+      square
+      variant="outlined"
+    >
+      <Container maxWidth="lg" sx={{ height: "544px" }}>
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+
+            my: 1,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-around",
+              gap: "25px",
+              width: "full",
+              height: "544px",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: "10px",
+                paddingTop: "100px",
+              }}
+            >
+              <Image
+                src={"/image.png"}
+                width={31.27}
+                height={26.77}
+                alt="imagedesc"
+              />
+              <Typography fontWeight={700} fontSize={20} color="text.secondary">
+                Food Delivery
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                gap: "10px",
+              }}
+            >
+              <Typography color="text.secondary">Home</Typography>
+              <Typography color="text.secondary">Холбоо барих</Typography>
+              <Typography color="text.secondary">Хоолны цэс</Typography>
+              <Typography color="text.secondary">
+                Үйлчилгээний нөхцөл
+              </Typography>
+              <Typography color="text.secondary">Хүргэлтийн бүс</Typography>
+              <Typography color="text.secondary">Нууцлалын бодлого</Typography>
+            </Box>
+            <Box
+              sx={{
+                flexGrow: 1,
+                justifyContent: "center",
+                display: "flex",
+                mb: 2,
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Typography variant="caption" color="text.secondary">
+                  © 2024 Pinecone Foods LLC
+                </Typography>
+                <Typography variant="caption" color="text.secondary">
+                  Зохиогчийн эрх хуулиар хамгаалагдсан
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </Paper>
+  );
+}
