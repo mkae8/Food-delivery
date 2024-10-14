26f63bfe89b1e4692cc3e4fb20ddfca1cd1c0a8c
battserens <battseren.s@gmail.com>
2024-10-14 09:25:58 +0800

    homepage infocard component nemev

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,26 +1,17 @@
 import React, { ChangeEvent } from "react";
-import { Input } from "@/components/Input"; 
 import { Typography } from "@mui/material";
+import { InfoCard } from "@/components/homepage/InfoCard";
+import { FiBookOpen } from "react-icons/fi";
+
+import { green } from "@mui/material/colors";
+import HomeInfoCards from "@/components/homepage/InfoCards";
 
 export default function Home() {
-  const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value); 
-  };
-
   return (
     <div>
-      <Input
-        id="password"
-        label={"Нууц үг"}
-        variant="filled"
-        
-      />
-      <Input
-        id="name"
-        label="Нэрээ оруулна уу"
-        variant="filled"
-        
-      />
+      <div>
+        <HomeInfoCards />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/homepage/InfoCards.tsx
@@ -0,0 +1,51 @@
+import React, { ChangeEvent } from "react";
+import { Typography } from "@mui/material";
+import { InfoCard } from "@/components/homepage/InfoCard";
+import { FiBookOpen } from "react-icons/fi";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { PiBowlFood } from "react-icons/pi";
+
+import { green } from "@mui/material/colors";
+import { CgEnter } from "react-icons/cg";
+
+export default function HomeInfoCards() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "18px",
+        justifyContent: "center",
+        marginTop: "20px",
+      }}
+    >
+      <div>
+        <InfoCard
+          Icon={() => <FiBookOpen />}
+          text_main="Хүргэлтийн төлөв хянах"
+          text_description="Захиалга бэлтгэлийн явцыг хянах"
+        />
+      </div>
+      <div>
+        <InfoCard
+          Icon={() => <MdOutlineWatchLater />}
+          text_main="Шуурхай хүргэлт"
+          text_description="Захиалга бэлтгэлийн явцыг хянах"
+        />
+      </div>
+      <div>
+        <InfoCard
+          Icon={() => <PiBowlFood />}
+          text_main="Эрүүл, баталгаат орц"
+          text_description="Захиалга бэлтгэлийн явцыг хянах"
+        />
+      </div>
+      <div>
+        <InfoCard
+          Icon={() => <FiBookOpen />}
+          text_main="Хоолны өргөн сонголт"
+          text_description="Захиалга бэлтгэлийн явцыг хянах"
+        />
+      </div>
+    </div>
+  );
+}
