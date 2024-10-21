97f1c0f24685424807974695a17298662268d0f2
24LP1202 24LP1202 <24LP1202@G302-CC0088.local>
2024-10-21 11:22:29 +0800

    food add completed

--- server/routers/users/userRouter.ts
+++ server/routers/users/userRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { signUpController } from "../../controllers/usersController/signUpController";
+import { foodController } from "../../controllers/foodController/foodController"
 
 const userRouter = Router();
 
 userRouter.route("/user/signup").post(signUpController);
+userRouter.route("/admin/category").post(foodController)
 
 export default userRouter;
