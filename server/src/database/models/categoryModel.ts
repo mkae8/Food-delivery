218d7f172d4b1840c9c375254fc6adacab48dc2b
Bilguun <Bilguun040728@gmail.com>
2024-10-23 12:55:42 +0800

    categoyr nemdeg bolson mongodb ruu

--- server/src/database/models/categoryModel.ts
+++ server/src/database/models/categoryModel.ts
@@ -1,19 +1,47 @@
-import { Model, Schema, models, model, ObjectId, Types } from "mongoose";
+// import { Model, Schema, models, model, Types } from "mongoose";
+
+// export type CategoryModelType = {
+//   _id: Types.ObjectId;
+//   categoryName: string;
+//   foodId: Types.ObjectId[];
+//   createdAt: Date;
+//   updatedAt: Date;
+// };
+
+// const CategorySchema = new Schema<CategoryModelType>({
+//   categoryName: { type: String, required: false },
+//   foodId: { type: [Types.ObjectId], required: true },
+//   createdAt: { type: Date, default: Date.now, required: true, immutable: true },
+//   updatedAt: { type: Date, default: Date.now, required: true },
+// });
+
+// export const CategoryModel: Model<CategoryModelType> =
+//   models["Category"] || model<CategoryModelType>("Category", CategorySchema);
+
+import { Model, Schema, models, model, Types } from "mongoose";
 
 export type CategoryModelType = {
   _id: Types.ObjectId;
-  categoryName: string;
+  categoryName?: string;
   foodId: Types.ObjectId[];
   createdAt: Date;
   updatedAt: Date;
 };
 
-const CategorySchema = new Schema<CategoryModelType>({
-  categoryName: { type: String, required: false },
-  foodId: { type: [Types.ObjectId], required: true },
-  createdAt: { type: Date, default: Date.now, required: true, immutable: true },
-  updatedAt: { type: Date, default: Date.now, required: true },
-});
+const CategorySchema = new Schema<CategoryModelType>(
+  {
+    categoryName: { type: String, unique: true, sparse: true },
+    foodId: { type: [Types.ObjectId], required: true, ref: "Food" },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      required: true,
+      immutable: true,
+    },
+    updatedAt: { type: Date, default: Date.now, required: true },
+  },
+  { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+);
 
 export const CategoryModel: Model<CategoryModelType> =
   models["Category"] || model<CategoryModelType>("Category", CategorySchema);
