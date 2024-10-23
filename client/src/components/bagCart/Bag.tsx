2669a80a820b316b0bc21d56cd2148a87ce7f8a8
24LP1202 24LP1202 <24LP1202@G302-CC0141.local>
2024-10-23 19:55:51 +0800

    not yet

--- client/src/components/bagCart/Bag.tsx
+++ client/src/components/bagCart/Bag.tsx
@@ -15,81 +15,95 @@ export const Bag = () => {
     <div
       style={{
         display: isSidebarVisible ? "flex" : "none",
-        position: "fixed",
+        position: "absolute",
         zIndex: "100",
-        width: "100vw",
-        height: "100vh",
-      }}>
+        width: "100%",
+        height: "100%",
+        color: "black",
+      }}
+    >
       <div
         style={{
           width: "65%",
           backgroundColor: "#00000080",
           opacity: "50%",
-        }}></div>
+        }}
+      ></div>
       <div
         style={{
           width: "35%",
           backgroundColor: "white",
           right: "0",
-        }}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}>
-          <Button
-            onClick={toggleBag}
-            style={{ width: "48px", height: "48px", padding: "0px" }}>
-            <KeyboardArrowLeftIcon />
-          </Button>
-          <p
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <div
             style={{
               display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}>
-            Таны сагс
-          </p>
-          <p style={{ width: "50px" }} />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            margin: "16px",
-            paddingTop: "16px",
-            gap: "16px",
-            borderTop: "1px solid #D6D8DB",
-          }}>
-          <BagCart />
+              justifyContent: "space-between",
+            }}
+          >
+            <Button
+              onClick={toggleBag}
+              style={{ width: "48px", height: "48px", padding: "0px" }}
+            >
+              <KeyboardArrowLeftIcon />
+            </Button>
+            <p
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Таны сагс
+            </p>
+            <p style={{ width: "50px" }} />
+          </div>
+          <Box
+            sx={{
+              margin: "16px 0 16px px",
+              borderBottom: " 1px solid #D6D8DB",
+              borderTop: "1px solid #D6D8DB",
+            }}
+          >
+            <BagCart />
+            <BagCart />
+          </Box>
         </div>
         <Box
           sx={{
             display: "flex",
             justifyContent: "space-between",
+            alignItems: "center",
             height: "172px",
             gap: "10px",
             paddingX: "32px",
             paddingTop: "10px",
             paddingBottom: "30px",
             width: "full",
-            position: "fixed",
-            bottom: "0px",
-          }}>
+
+            boxShadow: "3",
+          }}
+        >
           <Box
             sx={{
               display: "flex",
               flexDirection: "column",
               justifyContent: "center",
-              alignItems: "center",
+              alignItems: "start",
               width: "50%",
-            }}>
+            }}
+          >
             <Typography>Таны нийт төлөх дүн</Typography>
             <Typography sx={{ fontWeight: "700", fontSize: "18px" }}>
               {"Total"}
             </Typography>
           </Box>
-          <Button sx={{ width: "50%" }} variant="contained">
+          <Button sx={{ width: "50%", height: "48px" }} variant="contained">
             Захиалах
           </Button>
         </Box>
