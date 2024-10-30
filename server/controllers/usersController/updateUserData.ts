1dcbc4563cf6a20c49d8b866ace8b74320ccb74a
mkae8 <ntemka93@gmail.com>
2024-10-30 13:41:18 +0800

    userProfile backend hiisen

---
+++ server/controllers/usersController/updateUserData.ts
@@ -0,0 +1,16 @@
+import { UserModel } from "../../src/database/models/userModel";
+
+export const updateUserData = async (req: any, res: any) => {
+  const { userId } = res.locals;
+  const { username, phoneNumber, email } = req.body;
+  try {
+    const userUpdate = await UserModel.findByIdAndUpdate(userId, {
+      username,
+      phoneNumber,
+      email,
+    });
+    res.send({ message: "Update successfully" }).status(201);
+  } catch (error) {
+    res.send("boldoggui ee bro").status("400");
+  }
+};
